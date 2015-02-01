6e6389660baf596eb611df035ae62ab6d4d22cfa
Alex Ngo <alex_ngo@live.com>
2015-01-31 21:34:27 -0800

    gets data for race, asians, and gender

--- app/app.js
+++ app/app.js
@@ -9,42 +9,68 @@ var states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorad
 "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico",
 "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island",
 "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington",
-"West Virginia", "Wisconsin", "Wyoming"];
+"West Virginia", "Wisconsin", "Wyoming", "United States"];
 
 angular.module('censusVisuals', [
 ])
-    .controller('ProjectController', function ($scope) {
-        $scope.baselink = "http://api.census.gov/data/2000/sf1?key="
+    .controller('VisualsController', function ($scope) {
+        $scope.states = states;
+        $scope.button1 = "All States"
+        $scope.stateSwitch = function(state){
+            createGraph(genderDictList, state);
+        }
+        $scope.changeButtonText = function(button, state){
+            $scope[button] = state;
+        }
     });
 
-//get men, women population 1990
-var gender1990;
-var gender2000;
-var gender2010;
+//get men, women population 1990, 2000, 2010
+var genderDict1990;
+var genderDict2000;
+var genderDict2010;
 $.getJSON(baselink1990 + "&get=P0050001,P0050002&for=state:*", function (data) {
-    gender1990 = data;
-});
-$.getJSON(baselink2000 + "&get=P012002,P012026&for=state:*", function (data) {
-    gender2000 = data;
-});
-$.getJSON(baselink2010 + "&get=P0120002,P0120001&for=state:*", function (data) {
-    gender2010 = data;
+    genderDict1990 = getGenderDict(data, 1990);
 });
 
-var stateGender = {};
-var totals = [0, 0];
-var getGenders = function () {
-    setTimeout(function (data) {
-        for (var i = 1; i < 52; i++) {
-            var gender = [];
-            var males = parseInt(gender2010[i][0]);
-            var females = parseInt(gender2010[i][1]) - males;
-            gender[0] = males;
-            gender[1] = females;
-            totals[0] += males;
-            totals[1] += females;
-            stateGender[states[i - 1]] = gender;
-        }
-    }, 1000);
-}
-var gender
+$.getJSON(baselink2000 + "&get=P012002,P012026&for=state:*", function (data) {
+    genderDict2000 = getGenderDict(data, 2000);
+});
+
+$.getJSON(baselink2010 + "&get=P0120002,P0120001&for=state:*", function (data) {
+    genderDict2010 = getGenderDict(data, 2010);
+});
+
+var raceDict1990;
+var raceDict2000;
+var raceDict2010;
+$.getJSON(baselink1990 + "&get=P0060001,P0060002,P0060003,P0060004,P0060005&for=state:*", function (data) {
+    raceDict1990 = getRaceDict(data, true);
+});
+
+$.getJSON(baselink2000 + "&get=P003003,P003004,P003005,P003006,P003007,P003008,P003009&for=state:*", function (data) {
+    raceDict2000 = getRaceDict(data, false);
+});
+
+$.getJSON(baselink2010 + "&get=P0030002,P0030003,P0030004,P0030005,P0030006,P0030007,P0030008&for=state:*", function (data) {
+    raceDict2010 = getRaceDict(data, false);
+});
+
+var asianDict1990;
+var asianDict2000;
+var asianDict2010;
+$.getJSON(baselink1990 + "&get=P0070003,P0070004,P0070005,P0070006,P0070007,P0070008,P0070009,P0070010,P0070011,P0070012,P0070013,P0070014,P0070015,P0070016&for=state:*", function (data) {
+    asianDict1990 = getAsianDict(data, 1990);
+});
+$.getJSON(baselink2000 + "&get=PCT007002,PCT007003,PCT007004,PCT007005,PCT007006,PCT007007,PCT007008,PCT007009,PCT007010,PCT007011,PCT007012,PCT007013,PCT007014,PCT007015,PCT007016,PCT007017,PCT007019&for=state:*", function (data) {
+    asianDict2000 = getAsianDict(data, 2000);
+});
+$.getJSON(baselink2010 + "&get=PCT0070002,PCT0070003,PCT0070004,PCT0070005,PCT0070006,PCT0070007,PCT0070008,PCT0070009,PCT0070010,PCT0070011,PCT0070012,PCT0070013,PCT0070014,PCT0070015,PCT0070016,PCT0070017,PCT0070018,PCT0070019,PCT0070020,PCT0070022&for=state:*", function (data) {
+    asianDict2010 = getAsianDict(data, 2010);
+});
+
+setTimeout(function () {
+    var asianDictList = [asianDict1990, asianDict2000, asianDict2010];
+    var genderDictList = [genderDict1990, genderDict2000, genderDict2010];
+    var raceDictList = [raceDict1990, raceDict2000, raceDict2010];
+    createGraph(genderDictList, "total");
+}, 300);
